e3aac8e42a70813d37f4f0d5d4224878916585ee
Sri ram <upsharlhis@gmail.com>
2025-03-02 15:10:39 +0530

    Build Error Solved

--- src/components/maps.tsx
+++ src/components/maps.tsx
@@ -1,51 +1,45 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
+import "leaflet/dist/leaflet.css";
+
+// Dynamically import Leaflet and react-leaflet (avoiding SSR issues)
+const MapContainer = dynamic(() => import("react-leaflet").then(m => m.MapContainer), { ssr: false });
+const TileLayer = dynamic(() => import("react-leaflet").then(m => m.TileLayer), { ssr: false });
+const Marker = dynamic(() => import("react-leaflet").then(m => m.Marker), { ssr: false });
+const Popup = dynamic(() => import("react-leaflet").then(m => m.Popup), { ssr: false });
 
 export default function MapComponent() {
-  const [mapIsReady, setMapIsReady] = useState(false);
+  const [L, setLeaflet] = useState<any>(null);
 
   useEffect(() => {
-    const initializeMap = async () => {
-      const L = await import('leaflet');
-      const { MapContainer, TileLayer, Marker, Popup } = await import('react-leaflet');
-      
-      await import('leaflet/dist/leaflet.css');
-      
-      delete L.Icon.Default.prototype._getIconUrl;
-      L.Icon.Default.mergeOptions({
+    import("leaflet").then((leaflet) => {
+      setLeaflet(leaflet);
+
+      // Fix default marker icon issue
+      leaflet.Icon.Default.mergeOptions({
         iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
         iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
         shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png"
       });
-      
-      setMapIsReady(true);
-    };
-
-    initializeMap();
+    });
   }, []);
 
-  if (!mapIsReady) {
-    return (
-      <div className="h-[400px] w-full flex items-center justify-center bg-gray-100">
-        Loading map...
-      </div>
-    );
-  }
+  const position: [number, number] = [28.6139, 77.2090]; // New Delhi
 
-  const MapContent = () => {
-    const { MapContainer, TileLayer, Marker, Popup } = require('react-leaflet');
-    const position = [28.6139, 77.2090];
-    
-    return (
-      <MapContainer center={position} zoom={13} style={{ height: "400px", width: "100%" }}>
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={position}>
-          <Popup>Welcome to New Delhi!</Popup>
-        </Marker>
-      </MapContainer>
-    );
-  };
-
-  return <MapContent />;
+  return (
+    <div className="h-[400px] w-full">
+      {L ? (
+        <MapContainer center={position} zoom={13} style={{ height: "100%", width: "100%" }}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={position}>
+            <Popup>Welcome to New Delhi!</Popup>
+          </Marker>
+        </MapContainer>
+      ) : (
+        <div className="h-full flex items-center justify-center bg-gray-100">Loading map...</div>
+      )}
+    </div>
+  );
 }
